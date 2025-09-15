3524efc3702c0bea20164a1b04ffb21617ee44e5
txasw <txasw@outlook.co.th>
2025-09-15 14:24:32 +0700

    cell03-ex02

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+input_str = input("What you gotta say? : ")
+while True:
+    input_str = input("I got that! Anything else? : ")
+    if input_str == "STOP":
+        break
