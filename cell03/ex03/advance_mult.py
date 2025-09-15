5099f2b235eaa693fcb4ac41f06e9b6171f5cc89
txasw <txasw@outlook.co.th>
2025-09-15 14:35:48 +0700

    fix cell03-ex03

--- cell03/ex03/advance_mult.py
+++ cell03/ex03/advance_mult.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-    
+
+# check for arguments
+import sys
+if len(sys.argv) != 1:
+    print("Error: No arguments needed")
+    sys.exit(1)
 for i in range(11):
     print(f"Table de {i} :", end="")
     for j in range(11):
