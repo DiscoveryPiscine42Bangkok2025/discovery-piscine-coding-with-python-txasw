115b8a8e4e50e06a864e0b1f65ff890a27c1a033
txasw <txasw@outlook.co.th>
2025-09-15 13:49:45 +0700

    cell02-ex00,ex01

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+num = input()
+if float(num) < 0:
+    print("This number is negative.")
+elif float(num) > 0:
+    print("This number is positive.")
+else:
+    print("This number is both positive and negative.")
+    
